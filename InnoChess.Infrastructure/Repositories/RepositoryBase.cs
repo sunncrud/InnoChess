87a9f97e58fa751f62c5bd8962017c359121cd47
SunCrud <sunncrud@gmail.com>
2026-01-22 15:50:33 +0300

    fix: rename db context variable

--- InnoChess.Infrastructure/Repositories/RepositoryBase.cs
+++ InnoChess.Infrastructure/Repositories/RepositoryBase.cs
@@ -5,56 +5,56 @@ using Microsoft.EntityFrameworkCore;
 
 namespace InnoChess.Infrastructure.Repositories
 {
-    public class RepositoryBase<TEntity>(InnoChessDbContext context) : IRepositoryBase<TEntity>
+    public class RepositoryBase<TEntity>(InnoChessDbContext _context) : IRepositoryBase<TEntity>
         where TEntity : class, IEntity
     { 
         public async Task<List<TEntity>> GetAllAsync(CancellationToken cancellationToken)
         {
-            return await context.Set<TEntity>()
+            return await _context.Set<TEntity>()
                 .AsNoTracking()
                 .ToListAsync(cancellationToken);
         }
 
         public IQueryable<TEntity> GetQueryable()
         {
-            return context.Set<TEntity>().AsNoTracking();
+            return _context.Set<TEntity>().AsNoTracking();
         }
         
         public async Task<TEntity?> GetByIdAsync(Guid id, CancellationToken cancellationToken)
         {
-            return await context.Set<TEntity>()
+            return await _context.Set<TEntity>()
                 .AsNoTracking()
                 .FirstOrDefaultAsync(x => x.Id.Equals(id), cancellationToken);
         }
 
         public async Task<Guid> CreateAsync(TEntity entity, CancellationToken cancellationToken)
         {
-            await context.Set<TEntity>()
+            await _context.Set<TEntity>()
                 .AddAsync(entity, cancellationToken);
-            await context
+            await _context
                 .SaveChangesAsync(cancellationToken);
             return entity.Id;
         }
         public async Task<Guid?> UpdateAsync(TEntity entity, CancellationToken cancellationToken)
         {
-            context.Set<TEntity>()
+            _context.Set<TEntity>()
                 .Update(entity);
-            await context.SaveChangesAsync(cancellationToken);
+            await _context.SaveChangesAsync(cancellationToken);
             return entity.Id;
         }
 
         public async Task<Guid?> DeleteAsync(Guid id, CancellationToken cancellationToken)
         {
             var entity = await GetByIdAsync(id, cancellationToken);
-            if (entity != null) context.Set<TEntity>()
+            if (entity != null) _context.Set<TEntity>()
                 .Remove(entity);
-            await context.SaveChangesAsync(cancellationToken);
+            await _context.SaveChangesAsync(cancellationToken);
             return entity.Id;
         }
 
         public async Task SaveAsync(CancellationToken cancellationToken)
         {
-            await context
+            await _context
                 .SaveChangesAsync(cancellationToken);
         } 
     }
